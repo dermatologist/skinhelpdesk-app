2ef603c5ee1f1cf4d06f0c5e4ca9ecd744cb31fc
Bell Eapen <github_public@gulfdoctor.net>
2021-05-26 11:40:25 -0400

    Add welcome screen

---
+++ lib/config/skinhelpdesk_config.dart
@@ -0,0 +1,4 @@
+final Map<String, String> skinhelpdeskConfig = {
+  "shdtone": "https://gulfdoctor.net/api/cgk/skinhelpdesk/v1/service/shdtone/-155179071",
+  "mainTitle": 'SkinHelpDesk.com demo App',
+};
