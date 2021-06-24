31752b6bba35d159ec4b61ff13e5ad7dd03fd215
Bell Eapen <github_public@gulfdoctor.net>
2021-06-24 12:44:37 -0400

    fix service

--- lib/config/skinhelpdesk_config.dart
+++ lib/config/skinhelpdesk_config.dart
@@ -1,6 +1,6 @@
 final Map<String, String> skinhelpdeskConfig = {
   "shdurl": "skinhelpdesk.p.rapidapi.com",
-  "service": "shdtone",
+  "service": "/shdtone",
   "apikey": "ABCD",
   "title": 'SkinHelpDesk.com demo App',
 };
